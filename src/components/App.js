da76677530c3021c91253c329c348facf882d7d5
Luis Ascencio <luismiguelascencio@gmail.com>
2019-09-20 11:00:55 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,81 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: "all"
       }
-    }
+    };
   }
+
+  onAdoptPet = id => {
+    let newPetArray = this.state.pets.map(pet => {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet;
+    });
+    console.log(newPetArray);
+    this.setState({
+      pets: newPetArray
+    });
+  };
+
+  /// Change the filter::
+  onChangeType = e => {
+    console.log("onchangetype target:", e.target.value);
+    this.setState({
+      pets: [],
+      filters: {
+        type: e.target.value
+      }
+    });
+  };
+  // Fetching :::
+  onFindPetsClick = () => {
+    if (this.state.filters.type === "all") {
+      return fetch("/api/pets").then(response => {
+        response.json().then(data => {
+          this.setState({
+            pets: data
+          });
+
+          console.log(this.state);
+        });
+      });
+    } else if (this.state.filters.type === "cat") {
+      return fetch("/api/pets?type=cat").then(response => {
+        response.json().then(data => {
+          this.setState({
+            pets: data
+          });
+          console.log(this.state);
+        });
+      });
+    } else if (this.state.filters.type === "dog") {
+      return fetch("/api/pets?type=dog").then(response => {
+        response.json().then(data => {
+          this.setState({
+            pets: data
+          });
+          console.log(this.state);
+        });
+      });
+    } else if (this.state.filters.type === "micropig") {
+      return fetch("/api/pets?type=micropig").then(response => {
+        response.json().then(data => {
+          this.setState({
+            pets: data
+          });
+          console.log(this.state);
+        });
+      });
+    }
+  };
 
   render() {
     return (
@@ -24,16 +86,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onFindPetsClick={this.onFindPetsClick}
+                onChangeType={this.onChangeType}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
