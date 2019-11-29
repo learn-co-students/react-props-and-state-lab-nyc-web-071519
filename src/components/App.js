ca065d09cf44a5546e1a208f2be5ed2186477bbe
thenatural86 <philip.smalls86@gmail.com>
2019-11-29 17:58:13 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: "all"
       }
-    }
+    };
   }
+
+  onChangeType = ({ target: { value } }) => {
+    console.log({ target: value });
+    this.setState({ filters: { ...this.state.filters, type: value } });
+  };
+
+  onAdoptPet = petId => {
+    console.log("click");
+    let pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet;
+    });
+    this.setState({ pets });
+  };
+
+  onFindPetsClick = () => {
+    if (this.state.filters.type === "all") {
+      fetch("/api/pets")
+        .then(resp => resp.json())
+        .then(data => {
+          // console.log(data)
+          this.setState({ pets: data });
+        });
+    } else if (this.state.filters.type === "cat") {
+      fetch("/api/pets?type=cat")
+        .then(resp => resp.json())
+        .then(data => {
+          // console.log(data)
+          this.setState({ pets: data });
+        });
+    } else if (this.state.filters.type === "dog") {
+      fetch("/api/pets?type=dog")
+        .then(resp => resp.json())
+        .then(data => {
+          // console.log(data)
+          this.setState({ pets: data });
+        });
+    } else if (this.state.filters.type === "micropig") {
+      fetch("/api/pets?type=micropig")
+        .then(resp => resp.json())
+        .then(data => {
+          // console.log(data)
+          this.setState({ pets: data });
+        });
+    }
+  };
 
   render() {
     return (
@@ -24,16 +69,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
